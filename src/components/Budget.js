43a96879035acae947fd45b327f2bcf17618afdf
LokLokLokLam <loklokloklam@gmail.com>
2023-03-30 05:34:00 -0400

    React Final Project
    Signed-off-by: LokLokLokLam <loklokloklam@gmail.com>

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,44 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, expenses, dispatch, currency } = useContext(AppContext);
+
+    const changeBudget = (event) => {
+        let newBudget = event.target.value;
+        //Budget upper limit = 20000
+        if (newBudget > 20000)
+        {
+            alert("The Budget cannot exceed £20000");
+            return;
+        }
+
+        //Budget cannot lower than the spending
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total = total + item.cost);
+        }, 0);
+        if (newBudget < totalExpenses)
+        {
+            alert("The Budget cannot lower than the spending");
+            return;
+        }
+
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: parseInt(newBudget)
+        });
+    };
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency}&nbsp;</span>
+            <input
+                        type='number'
+                        step='10'
+                        value={budget}
+                        style={{ size: 10}}
+                        onChange={changeBudget}>
+            </input>
+            
         </div>
     );
 };
---
+++ src/components/Currency.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext'; 
+
+const Currency = () => {
+    const { dispatch } = useContext(AppContext);
+
+    const changeCurrency = (event) => {
+        let newCurrency = event.target.value;
+
+        dispatch ({
+            type: 'CHG_CURRENCY',
+            payload: newCurrency
+        });
+    }
+
+    return (
+        <div className="input-group mb-3">
+            <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+                <label className="input-group-text" htmlFor="selectCurrency">Currency</label>
+            </div>
+            <select className="custom-select" id="selectCurrency" onChange={changeCurrency}>
+                <option value="$" name="Dollar">$ Dollar</option>
+                <option selected="selected" value="£" name="Pound">£ Pound</option>
+                <option value="€" name="Euro">€ Euro</option>
+                <option value="₹" name="Ruppee">₹ Ruppee</option>
+            </select>
+        </div>
+    );
+}
+
+export default Currency;
